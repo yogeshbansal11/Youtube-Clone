2cfe5ec07affb5def98ec5b99aa80edb9ecf5f73
yogesh bansal <bansalindia850@gmail.com>
2024-12-03 23:11:14 +0530

    Added Feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
 
 import thumbnail1 from "../../assets/thumbnail1.png";
@@ -10,8 +10,23 @@ import thumbnail6 from "../../assets/thumbnail6.png";
 import thumbnail7 from "../../assets/thumbnail7.png";
 import thumbnail8 from "../../assets/thumbnail8.png";
 import { Link } from "react-router-dom";
+import { API_KEY } from "../../data";
 
-const Feed = () => {
+const Feed = ({category}) => {
+
+  const [data,setData] = useState([]);
+
+  const fetchData = async()=>{
+    const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+
+    //doubt
+    await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data.items))
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[category])
+
   return (
     <div className="feed">
       <Link to={`video/20/4321`} className="card">
